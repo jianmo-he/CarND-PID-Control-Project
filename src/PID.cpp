cf5e138dbd55df9567bbf625231e95b464685b6e
jianmo-he <jianmo.he@outlook.com>
2019-03-04 04:42:02 +0000

    copy repo to personal github

---
+++ src/PID.cpp
@@ -0,0 +1,34 @@
+#include "PID.h"
+
+/**
+ * TODO: Complete the PID class. You may add any additional desired functions.
+ */
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+  /**
+   * TODO: Initialize PID coefficients (and errors, if needed)
+   */
+  this->Kp = Kp_;
+  this->Ki = Ki_;
+  this->Kd = Kd_;
+  this->p_error = 0.0;
+  this->i_error = 0.0;
+  this->d_error = 0.0;
+}
+
+void PID::UpdateError(double cte) {
+  this->d_error = cte - this->p_error;
+  this->p_error = cte;
+  this->i_error += cte;
+}
+
+double PID::TotalError() {
+  /**
+   * TODO: Calculate and return the total error
+   */
+  return -Kp * this->p_error - Kd * this->d_error - Ki * this->i_error;
+}
